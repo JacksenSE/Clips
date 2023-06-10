c6cdcea66e2eb2e5ef2a3ba9cf69bca06de13703
JacksenSE <109568831+JacksenSE@users.noreply.github.com>
2023-06-10 14:37:47 -0700

    vercel deployment changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const apiRouter = require('./api');
+const path = require('path');
 
 const app = express();
-const port = 4005; // Replace with your desired port number
+const port = 4005; 
 
-// Other middleware and configurations...
+
 app.use(cors());
 
 app.use('/api', apiRouter);
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
