4a0ac9edc52a83b2a2d3b359ea9e8e84d7d9ed87
JacksenSE <109568831+JacksenSE@users.noreply.github.com>
2023-06-13 14:29:10 -0700

    dep13

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,18 +1,31 @@
-import { BsUpload } from 'react-icons/bs'
+import { Link } from 'react-router-dom';
+import { BsUpload } from 'react-icons/bs';
+
 function Nav() {
-    return(
-<div className="Nav">
-<ul>
-<li><a href="/"className="Home">Home</a></li>
-<li ><a href="/League"className="League">League</a></li>
-<li><a href="/Overwatch" className="Overwatch">Overwatch</a></li>
-<li><a href="/Valorant" className="Valorant">Valorant</a></li>
-<li><a href="/Misc" className="Misc">Misc</a></li>
-<li ><a href="/Upload" className="Upload"><BsUpload/></a></li>
+  return (
+    <div className="Nav">
+      <ul>
+        <li>
+          <Link to="/" className="Home">Home</Link>
+        </li>
+        <li>
+          <Link to="/League" className="League">League</Link>
+        </li>
+        <li>
+          <Link to="/Overwatch" className="Overwatch">Overwatch</Link>
+        </li>
+        <li>
+          <Link to="/Valorant" className="Valorant">Valorant</Link>
+        </li>
+        <li>
+          <Link to="/Misc" className="Misc">Misc</Link>
+        </li>
+        <li>
+          <Link to="/Upload" className="Upload"><BsUpload /></Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
-</ul>
-</div>
-)}
-
-
-export default Nav
+export default Nav;
