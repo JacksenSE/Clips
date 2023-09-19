b1d21b1f991fedd7d7c3afe0f1f85c536139f917
JacksenSE <109568831+JacksenSE@users.noreply.github.com>
2023-09-18 22:25:01 -0700

    FINSIHED4

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4005',
+        target: 'http://cfc555.ddns.net:74/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
