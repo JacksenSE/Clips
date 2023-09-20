3657fadb9a191c036d5485aec653615da803d6a8
JacksenSE <109568831+JacksenSE@users.noreply.github.com>
2023-09-19 18:57:30 -0700

    Changed Upload button to Upload rather than select video2

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -3,10 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 import { Container, Form, Row, Col, Card } from 'react-bootstrap';
 import Nav from '../components/nav';
 import { Link } from 'react-router-dom';
-const CDNURL = "https://cfc555.ddns.net/api/videos/";
+const CDNURL = "http://localhost:4005/api/videos/";
 
 function Upload() {
   const [videos, setVideos] = useState([]);
+
 
   async function getVideos() {
     try {
@@ -20,13 +21,20 @@ function Upload() {
     }
   }
 
-  async function handleUpload() {
+  async function uploadFile(e) {
+    const videoFile = e.target.files[0];
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const category = document.getElementById("category").value;
 
+    if (videoFile.size > 200 * 1024 * 1024) {
+      alert("File size exceeds the maximum allowed (200MB). Please select a smaller file.");
+      return; // Prevent further execution
+    }
+
     try {
       const formData = new FormData();
+      formData.append('video', videoFile);
       formData.append('title', title);
       formData.append('author', author);
       formData.append('category', category);
@@ -47,45 +55,45 @@ function Upload() {
 
   return (
     <>
-      <Nav />
-
-      <Container className='mt-5 upload-container' style={{ backgroundColor: "gray" }}>
-        <h1 className='Video-Title'>Video Upload</h1>
-        <Form className="upload-form">
-          <div className="form-group">
-            <label htmlFor="title" className="form-label">Title</label>
-            <input type="text" id="title" name="title" className="form-input" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="author" className="form-label">Author</label>
-            <input type="text" id="author" name="author" className="form-input" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="category" className="form-label">Category</label>
-            <select id="category" name="category" className="form-input">
-              <option value="category1">League</option>
-              <option value="category2">Overwatch</option>
-              <option value="category3">Valorant</option>
-              <option value="category4">BattleBit Remastered</option>
-              <option value="category5">Rainbow 6 Siege</option>
-              <option value="category6">Apex Legends</option>
-              <option value="category7">CounterStrike 2</option>
-              <option value="category8">Misc</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="video" className="form-label">Video</label>
-            <label className="custom-file-upload">
-              <input type="file" id="video" name="video" accept="video/*" className="form-input" />
-              Select a Video
-            </label>
-          </div>
-          <button className="submit-button" onClick={handleUpload}>Upload</button>
-        </Form>
-      </Container>
+    <Nav />
+   
+    <Container className='mt-5 upload-container' style={{ backgroundColor: "gray" }}>
+      <h1 className='Video-Title'>Video Upload</h1>    
+     <Form className="upload-form">
+  <div className="form-group">
+    <label htmlFor="title" className="form-label">Title</label>
+    <input type="text" id="title" name="title" className="form-input" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="author" className="form-label">Author</label>
+    <input type="text" id="author" name="author" className="form-input" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="category" className="form-label">Category</label>
+    <select id="category" name="category" className="form-input">
+      <option value="category1">League</option>
+      <option value="category2">Overwatch</option>
+      <option value="category3">Valorant</option>
+      <option value="category4">BattleBit Remastered</option>
+      <option value="category5">Rainbow 6 Siege</option>
+      <option value="category6">Apex Legends</option>
+      <option value="category7">CounterStrike 2</option>
+      <option value="category8">Misc</option>
+    </select>
+  </div>
+  <div className="form-group">
+    <label htmlFor="video" className="form-label">Video</label>
+    <label className="custom-file-upload">
+      <input type="file" id="video" name="video" accept="video/*" className="form-input" onChange={(e) => uploadFile(e)} />
+      Select a Video
+    </label>
+  </div>
+  <Link to='/'><button className="submit-button"onClick={(e) => uploadFile(e)}>Upload</button> </Link>
+</Form>
+    </Container>
     </>
   );
-
+  
 }
 
 export default Upload;
